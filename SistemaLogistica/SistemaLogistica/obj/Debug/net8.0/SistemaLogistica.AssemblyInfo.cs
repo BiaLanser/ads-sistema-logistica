c5071f9bb11b1fdf451d323e4fc4929d3d4cb9d3
BiaLanser <biancalperes@gmail.com>
2025-03-28 13:49:31 -0300

    feat: structure

--- SistemaLogistica/SistemaLogistica/obj/Debug/net8.0/SistemaLogistica.AssemblyInfo.cs
+++ SistemaLogistica/SistemaLogistica/obj/Debug/net8.0/SistemaLogistica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaLogistica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83aa75f0d3e136557b88023a744fbb6873d48887")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaLogistica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaLogistica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
